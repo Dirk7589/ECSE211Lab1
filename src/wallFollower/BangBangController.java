585006b98ff58e788766c4a2ecaab7ef0dca0baf
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2016-09-29 12:18:46 -0400

    Updated documentation

--- src/wallFollower/BangBangController.java
+++ src/wallFollower/BangBangController.java
@@ -1,22 +1,34 @@
 package wallFollower;
 import lejos.hardware.motor.*;
 
+/**
+ * Implements a bang-bang controller based on input from the ultrasonic sensor
+ */
 public class BangBangController implements UltrasonicController{
 	private final int bandCenter, bandwidth;
 	private final int motorLow, motorHigh;
 	private int distance;
 	private EV3LargeRegulatedMotor leftMotor, rightMotor;
 	
+	/**Provides a bang-bang controller object capable of processing ultraSonic data
+	 * @param leftMotor The left motor object
+	 * @param rightMotor The right motor
+	 * @param bandCenter The center band
+	 * @param bandwidth The width of the band
+	 * @param motorLow The lowest value of the motor
+	 * @param motorHigh The highest value of the motor
+	 */
 	public BangBangController(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor,
 							  int bandCenter, int bandwidth, int motorLow, int motorHigh) {
-		//Default Constructor
+		
 		this.bandCenter = bandCenter;
 		this.bandwidth = bandwidth;
 		this.motorLow = motorLow;
 		this.motorHigh = motorHigh;
 		this.leftMotor = leftMotor;
 		this.rightMotor = rightMotor;
-		leftMotor.setSpeed(motorHigh);				// Start robot moving forward
+		
+		leftMotor.setSpeed(motorHigh);
 		rightMotor.setSpeed(motorHigh);
 		leftMotor.forward();
 		rightMotor.forward();
